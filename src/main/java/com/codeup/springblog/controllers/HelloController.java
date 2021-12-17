708313bd3aefc43c1d962de035e9b9b518f0cca7
Mary White <mary.a.white28@gmail.com>
2021-12-16 21:02:40 -0500

    browsing through next lecture on Views, getting data from views

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -12,16 +13,23 @@ public class HelloController {
         return "Hello from Mary!";
     }
 
-    @GetMapping("/hello{name}") //name is a variable that we named name
-    @ResponseBody
-    public String sayHello(@PathVariable String name){
-        return "Hello there " + name + "!";
-    }
+//    @GetMapping("/hello{name}") //name is a variable that we named name
+//    @ResponseBody
+//    public String sayHello(@PathVariable String name){
+//        return "Hello there " + name + "!";
+//    }
 
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
     public String addOne(@PathVariable int number){
         return number + " plus one is " + (number + 1) + ".";
+    }
+
+    //---views---
+    @GetMapping("/hello/{name}")  //now if we visit /hello/Codeup, the page will fill in Codeup as the name in the hello.html
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 
 
--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -4,10 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//@Controller
+//public class HomeController {
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String hello(){
+//        return "This is the landing page!";
+//    }
+//}
+
+//to use views, we will remove the responseBody annotation and create a home.html
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    @ResponseBody
+    @GetMapping("/home") //the name needs to match the name of the html file in your resources/templates folder
     public String welcome(){
         return "home";
     }
