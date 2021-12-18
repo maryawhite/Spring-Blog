e0abdd8eb27cab907b1f15dededb1cc6606544b8
Mary White <mary.a.white28@gmail.com>
2021-12-18 18:01:27 -0500

    reviewing curriculum, started exercise one, not correct" " " x q quit exit "" git commit -m "error"

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Random;
+
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
 
     public String hello(){
-        return "Hello from Mary!";
+        return "Hello from Mary!";  //this is what displays when you visit /home
     }
 
 //    @GetMapping("/hello{name}") //name is a variable that we named name
@@ -30,6 +32,33 @@ public class HelloController {
     public String sayHello(@PathVariable String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(){
+        return "join";
+    }
+
+    @PostMapping ("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {  //the html input field has a name of cohort.
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");  //after you submit the form, it will display this string
+        return "join";
+    }
+
+
+    @GetMapping("/roll-dice")
+    public String showDiceForm() {
+        return "roll-dice";  //it looks like the return returns the html of that name
+    }
+
+    @PostMapping("/roll-dice")
+    public String userGuess(@RequestParam(name = "userGuess") String userGuess, Model model) {
+        Random random = new Random();
+        int int_random = random.nextInt(6) + 1;
+        model.addAttribute("userGuess", "You Guessed " + userGuess);
+        model.addAttribute("int_random", "The random number is " + int_random);  //this isn't showing up on the page...
+        return "roll-dice";
+
     }
 
 
