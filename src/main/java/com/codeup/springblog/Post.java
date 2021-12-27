36f9b6f8f1b513d79b4f36ab04b6f6fd2ee68627
Mary White <mary.a.white28@gmail.com>
2021-12-27 14:17:13 -0500

    added the PostRepository interface and updated the Post class with appropriate annotations and constructors and updated PostController

--- src/main/java/com/codeup/springblog/Post.java
+++ src/main/java/com/codeup/springblog/Post.java
@@ -1,12 +1,40 @@
 package com.codeup.springblog;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="posts")   //this will create a table named posts in the adlister_db
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, length = 100)
     private String title;
+    @Column(nullable = false)
     private String body;
+
+    //default constructor
+    public Post() {
+    }
+
+    //2 constructors, one with id, one without id
+    public Post(Long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
 
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getTitle() {
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.Post;
+import com.codeup.springblog.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,16 +10,32 @@ import java.util.ArrayList;
 
 @Controller
 public class PostController {
-    @RequestMapping(path = "/posts/index", method = RequestMethod.GET)   //or use @GetMapping("/posts/index")
-    public String postsIndex(Model model){
-        ArrayList<Post> posts = new ArrayList<>();
-        Post post1 = new Post("title1", "body1");
-        Post post2 = new Post("title2", "body2");
-        posts.add(post1);
-        posts.add(post2);
-        model.addAttribute("posts", posts); //posts is the name of the arrayList, now we can iterate through posts on the index.html
-        return "/posts/index";
+
+    //dependency injection, create a Repository instance and initialize it in the controller class constructor
+    private final PostRepository postRepository;
+
+    //do I need @Autowired here?
+    public PostController(PostRepository postRepository) {  //name postRepository i.e. adDao or postDao
+        this.postRepository = postRepository;
     }
+
+    @GetMapping("/posts/index")
+    public String postsIndex(Model model) {
+        model.addAttribute("posts", postRepository.findAll());
+        return "posts/index";
+    }
+
+
+//    @RequestMapping(path = "/posts/index", method = RequestMethod.GET)   //or use @GetMapping("/posts/index")  UPDATED above after adding PostRepository
+//    public String postsIndex(Model model){
+//        ArrayList<Post> posts = new ArrayList<>();
+//        Post post1 = new Post("title1", "body1");
+//        Post post2 = new Post("title2", "body2");
+//        posts.add(post1);
+//        posts.add(post2);
+//        model.addAttribute("posts", posts); //posts is the name of the arrayList, now we can iterate through posts on the index.html
+//        return "/posts/index";
+//    }
 
 //    @RequestMapping(path= "/posts/{id}", method = RequestMethod.GET)
 //    @ResponseBody
