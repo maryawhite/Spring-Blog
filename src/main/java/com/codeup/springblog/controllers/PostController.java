a9e676b7803b6cea88589d7da1255655bf77693f
Mary White <mary.a.white28@gmail.com>
2021-12-27 20:42:36 -0500

    added repository annotation, added functionality to view the posts index, add a new post, and delete a post. Need to work on edit functionality

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -5,16 +5,15 @@ import com.codeup.springblog.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Optional;
 
 @Controller
 public class PostController {
-
     //dependency injection, create a Repository instance and initialize it in the controller class constructor
     private final PostRepository postRepository;
 
-    //do I need @Autowired here?
     public PostController(PostRepository postRepository) {  //name postRepository i.e. adDao or postDao
         this.postRepository = postRepository;
     }
@@ -52,15 +51,45 @@ public class PostController {
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-    @ResponseBody
+//    @ResponseBody
     public String viewCreatePost(){
-        return "This will be where you view the form for creating a post";
+//        return "This is where you view the form for creating a post";
+        return "/posts/create";
     }
 
-    @RequestMapping(path = "/posts/create", method = RequestMethod.POST) //or use @Postmapping
-    @ResponseBody
-    public String createPostForm(){
-        return "this will be the com.codeup.springblog.Post method to create a new post";
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST) //or use @Postmapping
+//    @ResponseBody
+//    public String createPostForm(){
+//        return "this will be the com.codeup.springblog.Post method to create a new post";
+//    }
+
+    @PostMapping("/posts/create")
+    public String addNewPost(@ModelAttribute Post post) {  //this is @ModelAttribute, NOT @RequestBody
+        postRepository.save(post);
+        return "redirect:/posts/index";
     }
+
+    //add edit functionality
+    @PostMapping("/posts/{postId}/edit")
+    public void editPost(@PathVariable("postId") Long postId, String title, String body) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalStateException("post with id " + postId + " not found."));
+
+        if(title != null && title.length() > 0 && !Objects.equals(post.getTitle(), title)) {
+            post.setTitle(title);
+        }
+        if(body != null && body.length() > 0 && !Objects.equals(post.getBody(), body)) {
+            post.setBody(body);
+        }
+    }
+
+
+    //add delete functionality--add a delete button in the show.html
+//    @DeleteMapping(path = "{postId}")
+    @PostMapping("/posts/index")
+    public void deletePost(Long postId) {
+        postRepository.deleteById(postId);
+    }
+
+
 
 }
